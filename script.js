0ec83719e372187ecf6644c079366c586472dd4e
Ayoub Baslam <abardayou@gmail.com>
2026-01-23 16:27:01 +0100

    '11'

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const burger = document.querySelector(".burger");
+const social = document.querySelector(".bottom-footer-social");
+const nav = document.querySelector("nav");
 
+nav.appendChild(social);
 burger.addEventListener("click", () => {
   console.log("ssss");
 });
-console.log("gggg");
