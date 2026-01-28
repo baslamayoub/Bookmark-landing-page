0603630bb31b6f4251e858b34ad45eeb96937e2d
Ayoub Baslam <abardayou@gmail.com>
2026-01-28 22:35:39 +0100

    hh

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const burger = document.querySelector(".burger");
+const logo = document.querySelector(".logo");
 const btnTabs = document.querySelectorAll(".btntab");
 const tabs = document.querySelectorAll("#tab");
-console.log(btnTabs[1]);
 
 const social = document.querySelector(".bottom-footer-social");
 const nav = document.querySelector("nav");
@@ -9,14 +9,22 @@ const nav = document.querySelector("nav");
 nav.appendChild(social);
 burger.addEventListener("click", () => {
   nav.classList.toggle("active");
+  logo.classList.toggle("active");
+  burger.classList.toggle("active");
 });
 
 btnTabs.forEach((el) => {
+  el.classList.remove("active");
   el.addEventListener("click", () => {
     console.log(Number(el.dataset.num));
     tabs.forEach((tab) => {
       tab.classList.remove("active");
     });
     tabs[Number(el.dataset.num)].classList.add("active");
+    btnTabs.forEach((el) => {
+      el.classList.remove("active");
+    });
+    btnTabs[Number(el.dataset.num)].classList.add("active");
+    console.log(Number(el.dataset.num) + el.classList);
   });
 });
